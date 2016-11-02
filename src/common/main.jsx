cfc30e2144157243b09a797248be51605029aeb6
Damon Toal-Rossi <damon@uber.com>
2016-11-02 16:45:24 -0600

    Clear out readme, add redux-logger

--- src/common/main.jsx
+++ src/common/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {applyMiddleware, createStore, combineReducers} from 'redux';
+import createLogger from 'redux-logger';
 import reduxThunk from 'redux-thunk';
 
 import '../stylesheets/main.scss';
@@ -11,9 +12,27 @@ import todos from './reducers/todos';
 import App from './containers/app.jsx';
 
 const rootReducer = combineReducers({todos});
+
+const logger = createLogger({
+  stateTransformer: (state) => {
+    return Object.keys(state).reduce((newState, key) => {
+      const store = state[key];
+      if (typeof store.toJS === 'function') {
+        return {
+          ...newState,
+          [key]: store,
+          [`${key}JS`]: store.toJS()
+        };
+      }
+
+      return {...newState, [key]: store};
+    }, {});
+  }
+});
+
 const store = createStore(
   rootReducer,
-  applyMiddleware(reduxThunk)
+  applyMiddleware(reduxThunk, logger)
 );
 
 ReactDOM.render(
